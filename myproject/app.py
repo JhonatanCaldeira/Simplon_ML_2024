d6c6723b1b2eb5bf0027253608a67322aa19f268
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2023-10-18 09:23:18 +0200

    Creating a standard structure for python projects

---
+++ myproject/app.py
@@ -0,0 +1,5 @@
+from components.mymodule import my_sum, my_concatenate
+
+print(my_sum(1,2,3,4,5))
+
+print(my_concatenate(a='Meu', b='nome', c='é', d='Jhonatan'))
---
+++ myproject/components/mymodule.py
@@ -0,0 +1,23 @@
+
+"""
+my_sum receives a list of arguments(args) and return its sum.
+"""
+def my_sum(*args):
+    result = 0
+
+    # Iterating over the Python args tuple
+    for x in args:
+        result += x
+    return result
+
+"""
+my_concatenate receives a list of named arguments(kwargs) and return
+a single concatenated string
+"""
+def my_concatenate(**kwargs):
+    result = ""
+    
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg + ' '
+    return result
