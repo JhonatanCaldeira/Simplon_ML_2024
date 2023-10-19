1234602d98dbeec7adf8dcf98bcd496893ff2f0b
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2023-10-19 11:46:08 +0200

    update

--- myproject/app.py
+++ myproject/app.py
@@ -1,6 +1,6 @@
 from components.mymodule import my_sum, my_concatenate
 from components.data_sc import data_load, data_clean_up, data_write_to_csv, DATA_FILE_IN
-from components.create_model import preprocessing_model, svc_model, DATA_FILE
+from components.create_model import preprocessing_model, svc_model, model_score, DATA_FILE
 
 # print(my_sum(1,2,3,4,5))
 
@@ -13,3 +13,4 @@ data_write_to_csv(df_titanic)
 df_titanic = data_load(DATA_FILE)
 preprocessing_model(df_titanic)
 svc_model()
+print(model_score())
--- myproject/components/create_model.py
+++ myproject/components/create_model.py
@@ -23,12 +23,21 @@ MODEL_FILEPATH = os.path.join(ML_DIR, MODEL_FILENAME)
 DATA_DIR = os.path.join(PARENT_DIR,'data')
 DATA_FILE = os.path.join(DATA_DIR, 'titanic_out.csv')
 
+"""
+Function to serializer one object using joblib method
+"""
 def joblib_serializer(object, filepath):
     joblib.dump(object, filepath)
 
+"""
+Function to unserializer one object using joblib method
+"""
 def joblib_deserializer(filepath):
     return joblib.load(filepath)
 
+"""
+Preprocessing function that encode a DataFrame using get_dummies methode
+"""
 def preprocessing_model(df: pd.DataFrame):
     columns = ["pclass","sex","sibsp","parch", "embarked"]
 
@@ -37,7 +46,11 @@ def preprocessing_model(df: pd.DataFrame):
 
     joblib_serializer(df, ENCODER_FILEPATH)
 
-def svc_model():
+"""
+Function that creates a DataFrame from a serialized encoder and return its
+X_train, X_teste, y_train and y_test
+"""
+def train_test():
     df = joblib_deserializer(ENCODER_FILEPATH)
 
     X = df.drop(columns=["survived"], axis=1)
@@ -46,19 +59,40 @@ def svc_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
                                                          train_size=42)
     
+    return X, y, X_train, X_test, y_train, y_test
+
+"""
+Apply SVM over the titanic data, recovered from the serialized encoder.
+Return a serialized model
+"""
+def svc_model():
+
+    X, y, X_train, X_test, y_train, y_test = train_test()
+    
     sc = StandardScaler()
     X = sc.fit_transform(X)
     X_train.iloc[:,:2] = sc.fit_transform(X_train.iloc[:,:2])
-    X_test.iloc[:,:2] = sc.transform(X_test.iloc[:,:2])
 
     classifier = SVC(kernel='rbf')
     classifier.fit(X_train, y_train)
 
     joblib_serializer(classifier, MODEL_FILEPATH)
 
-def model_analyser():
-    # classifier = joblib_deserializer(MODEL_FILEPATH)
-    pass
+"""
+Evaluate the Score from a serialized model.
+"""
+def model_score():
+    X, y, X_train, X_test, y_train, y_test = train_test()
+
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+    X_train.iloc[:,:2] = sc.fit_transform(X_train.iloc[:,:2])
+    X_test.iloc[:,:2] = sc.transform(X_test.iloc[:,:2])
+    
+    classifier = joblib_deserializer(MODEL_FILEPATH)
+
+    result = classifier.score(X_test,y_test)
+    return result
 
 if __name__ == '__main__':
     df_titanic = data_load(DATA_FILE)
