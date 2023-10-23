2d887a9d15de6ad3f95c9d4c9536db76661859d5
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2023-10-23 09:05:08 +0200

    Streamlit

--- 06-streamlit_project/pages/3_Synoidal_chart.py
+++ 06-streamlit_project/pages/3_Synoidal_chart.py
@@ -9,7 +9,7 @@ st.sidebar.markdown('# Visualisation de Signaux Sinusoïdaux')
 f = st.slider('Pick une fréquence',0, 1000, 440)
 
 N = 1
-t = np.linspace(0,1)
+t = np.linspace(0,0.1,4000)
 x = np.sin(2 * np.pi * f * t)
 
 st.line_chart(x)
